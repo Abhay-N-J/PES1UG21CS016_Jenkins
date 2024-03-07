8cc528b52bb5c213a9b11707bdc1fb0c48b493ca
Abhay NJ <abhayjoshi029@gmail.com>
2024-03-07 20:29:17 +0530

    Edited for webHook

--- main/main.cpp
+++ main/main.cpp
@@ -5,5 +5,6 @@ int main()
     std::cout << "Hello, world!" << std::endl;
     std::cout << "I am a cpp program \n";
     std::cout << "Editing the program\n";
+    std::cout << "For webhook\n";
     return 0;
 }
